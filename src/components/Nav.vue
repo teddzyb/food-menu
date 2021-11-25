4f7eb3da1652682de6a2c8e4ac0ab5c347839643
Edwin Bartlett <teddybartletg88@gmail.com>
2021-11-26 01:14:56 +0800

    Added Nav component

---
+++ src/components/Nav.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container-fluid bg-dark text-light">
+    <p class="fs-5">Food Menu</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nav",
+};
+</script>
